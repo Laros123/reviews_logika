7fae87b0fa05faab54010449fea31c4d293e92f7
Laros123 <you@example.com>
2024-03-02 12:38:47 +0200

    Created class Review

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    username = models.CharField(max_length=127)
+    text = models.TextField()
+    date= models.DateField(auto_now=True)
