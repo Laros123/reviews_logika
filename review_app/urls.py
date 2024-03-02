a3c02d70a80fe94b9624d80c9b067c754c44e8d1
Laros123 <you@example.com>
2024-03-02 15:11:01 +0200

    Created view and html review

--- review_app/urls.py
+++ review_app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reviews.views import reviews_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', reviews_list),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from reviews.models import Review
 
 # Create your views here.
+
+def reviews_list(request):
+    reviews = Review.objects.all()
+    context = {
+        'reviews': reviews
+    }
+    return render(request, template_name='reviews/base.html', context=context)
